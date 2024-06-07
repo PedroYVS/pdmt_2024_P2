886978efa7e807a2c6ab8bd81d7a1b6c4ebb6b8a
PedroYVS <pedyshib@gmail.com>
2024-06-07 17:38:46 -0300

    feat(p2-pdmt): adiciona conexao base axios, funcao para carregar fotos e component para apresentacao das fotos

--- App.tsx
+++ App.tsx
@@ -1,10 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import { FlatList, StyleSheet, Text, View, Image, Pressable } from 'react-native'
+import axios from 'axios'
+
+const theCatAPI = axios.create({
+  baseURL: 'https://api.thecatapi.com/v1/images/'
+})
+
+type Foto = {
+  id: number,
+  url: string,
+  width: number,
+  height: number,
+}
 
 export default function App() {
+  const [fotos, setFotos] = useState<Foto[]>([])
+
+  let indexes = 0
+
+  const carregaFotos = () => {
+    theCatAPI.get('/search?limit=5').then(response => {
+      const fotos: Foto[] = response.data.map((foto: any) => {
+        indexes++
+        return {
+          id: indexes,
+          url: foto.url,
+          width: foto.width,
+          height: foto.height,
+        }
+      })
+      setFotos(colecaoAtual => [...colecaoAtual, ...fotos])
+    })
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.appTitle}>Apresentando Fotos de Gatos</Text>
+      <Pressable onPress={carregaFotos} hitSlop={40}>
+        <Text>Carregar mais Fotos</Text>
+      </Pressable>
+      <FlatList
+      data={fotos}
+      keyExtractor={foto => String(foto.id)}
+      renderItem={foto => {
+        return(
+          <Image source={{uri: foto.item.url}} style={{width: foto.item.width, height: foto.item.height}}/>
+        )
+      }}/>
     </View>
   );
 }
